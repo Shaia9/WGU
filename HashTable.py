77b5e1a2c1ecbdb01d21c4075ef4175072b95d9e
brett <Brettshaia@gmail.com>
2025-02-14 15:08:51 -0600

    Started on HashTable.py

--- HashTable.py
+++ HashTable.py
@@ -4,37 +4,37 @@
 
 class HashTable:
     def __init__(self, start_capacity=10):
-        self.mapping = []
+        self.map = []
         for _ in range(start_capacity):
-            self.mapping.append([])
+            self.map.append([])
 
         # Hash Key Creation
         def hash_key(self, key):
-            return int(key) % len(self.mapping)
+            return int(key) % len(self.map)
 
         # Insert into Hash Table
         def insert(self, key, value):
             insert_hash = self.hash_key(key)
             insert_value = [key, value]
 
-            if self.mapping[insert_hash] == None:
-                self.mapping[insert_hash] = list([insert_value])
+            if self.map[insert_hash] == None:
+                self.map[insert_hash] = list([insert_value])
                 return True
             else:
-                for pair in self.mapping[insert_hash]:
+                for pair in self.map[insert_hash]:
                     if pair[0] == key:
                         pair[1] = insert_value
                         return True
-                self.mapping[insert_hash].append(insert_value)
+                self.map[insert_hash].append(insert_value)
                 return True
 
         # Updating package in Hash Table
         def update(self, key, value):
            insert_hash = self.hash_key(key)
-           if self.mapping(insert_hash) != None:
-               for pair in self.mapping[insert_hash]:
+           if self.map(insert_hash) != None:
+               for pair in self.map[insert_hash]:
                    if pair[0] == key:
-                       pair[1] == value
+                       var = pair[1] == value
                        print(pair[1])
                        return True
            else:
@@ -43,8 +43,8 @@ class HashTable:
         # Returning value from Hash Table
         def retrieve_value(self, key):
             insert_hash = self.hash_key(key)
-            if self.mapping(insert_hash) is not None:
-                for pair in self.mapping[insert_hash]:
+            if self.map(insert_hash) is not None:
+                for pair in self.map[insert_hash]:
                     if pair[0] == key:
                         return pair[1]
             return None
@@ -52,11 +52,11 @@ class HashTable:
         # Removing value from Hash Table
         def remove(self, key):
             insert_hash = self.hash_key(key)
-            if self.mapping(insert_hash) == None:
+            if self.map(insert_hash) == None:
                 return False
-            for i in range(0, len(self.mapping[insert_hash])):
-                if self.mapping[insert_hash][i][0] == key:
-                    self.mapping[insert_hash].pop(i)
+            for i in range(0, len(self.map[insert_hash])):
+                if self.map[insert_hash][i][0] == key:
+                    self.map[insert_hash].pop(i)
                     return True
                 return False
 
